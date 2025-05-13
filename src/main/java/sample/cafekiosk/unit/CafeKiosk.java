a947e614f8ab41de2e81890c632ad28732c974d4
sjin-hahn <sjin.hahn@samsung.com>
2025-05-13 21:50:53 +0900

    add Order and createOrder method of CafeKiosk

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,7 +1,9 @@
 package sample.cafekiosk.unit;
 
 import sample.cafekiosk.unit.beverage.Beverage;
+import sample.cafekiosk.unit.order.Order;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,6 +29,10 @@ public class CafeKiosk {
             totalPrice += beverage.getPrice();
         }
         return totalPrice;
+    }
+
+    public Order createOrder() {
+        return new Order(LocalDateTime.now(), beverages);
     }
 
 }

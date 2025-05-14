7ca60a257b44b776a1704935977e1257e4cfc58d
sjin-hahn <sjin.hahn@samsung.com>
2025-05-14 23:25:55 +0900

    isolate areas that are difficult to test

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -3,6 +3,9 @@ package sample.cafekiosk.unit;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
+import sample.cafekiosk.unit.order.Order;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -97,6 +100,52 @@ class CafeKioskTest {
 
         // Then
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @Test
+    void itMaybeCouldCreateOrder() {
+        // Given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        // When
+        Order order = cafeKiosk.createOrder(); // test 실행 시간에 따라 실패 할 수도 있음
+
+        // Then
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void itCouldAlwaysCreateOrderWithCurrentTime() {
+        // Given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        // When
+        Order order = cafeKiosk.createOrder(LocalDateTime.of(2025, 5, 14, 10, 0));
+
+        // Then
+        assertThat(order.getBeverages()).hasSize(1);
+        assertThat(order.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    @Test
+    void itCouldNotCreateOrderWithOutsideOpenTime() {
+        // Given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        cafeKiosk.add(americano);
+
+        // When & Then
+        assertThatThrownBy(()-> cafeKiosk.createOrder(LocalDateTime.of(2025, 5, 14, 9, 59)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("주문시간이 아닙니다. 관리자에게 문의하세요.");
     }
 
 }

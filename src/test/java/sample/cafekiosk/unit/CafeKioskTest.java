b328d41f170276fe5cbcd84bf960b8a94f8311bc
sjin-hahn <sjin.hahn@samsung.com>
2025-05-20 21:35:34 +0900

    refactor calculateTotalPrice method using itCalculateTotalPrice test

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -1,5 +1,6 @@
 package sample.cafekiosk.unit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
@@ -12,6 +13,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CafeKioskTest {
 
+//    @DisplayName("음료 1개 추가 테스트")
+    @DisplayName("음료 1개를 추가하면 주문 목록에 담긴다.")
     @Test
     void addManualTest() {
         // Given
@@ -100,6 +103,24 @@ class CafeKioskTest {
 
         // Then
         assertThat(cafeKiosk.getBeverages()).isEmpty();
+    }
+
+    @DisplayName("주문 목록에 담긴 삼품들의 총 금액을 계산할 수 있다.")
+    @Test
+    void itCalculateTotalPrice() {
+        // Given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+        Latte latte = new Latte();
+
+        cafeKiosk.add(americano);
+        cafeKiosk.add(latte);
+
+        // When
+        int totalPrice = cafeKiosk.calculateTotalPrice();
+
+        // Then
+        assertThat(totalPrice).isEqualTo(8500);
     }
 
     @Test
